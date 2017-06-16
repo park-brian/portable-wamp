2607fc2d62dd46d5e9067701ba3b0e19c43c21fc
Brian Park <re.public@outlook.com>
2017-06-15 20:33:25 -0400

    mysql: add scripts for starting daemon/shell

--- start_mysqld.sh
+++ start_mysqld.sh
@@ -2,6 +2,10 @@
 
 WORK_DIR="environment"
 export PATH=$PATH:$PWD/$WORK_DIR/httpd/bin:$PWD/$WORK_DIR/php:$PWD/$WORK_DIR/mysql/bin:$PWD/$WORK_DIR/bin
+echo "mysqld is now running [ctrl+c to exit]"
 
-## start mysql shell
-winpty mysql.exe -u root
+## start mysqld
+pushd $WORK_DIR/mysql/bin > /dev/null
+./mysqld.exe -u root --log_syslog=0
+popd
+
