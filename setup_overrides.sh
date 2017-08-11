325fedfcd50b5aefa233501df89cc69ad73b925e
park-brian <re.public@outlook.com>
2017-08-11 11:41:19 -0400

    remove setup_overrides.sh

--- setup_overrides.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WORK_DIR="environment"
-OVERRIDES_DIR="configuration"
-
-## copy default overrides
-echo "[COPY] $OVERRIDES_DIR/* [->] $WORK_DIR/"
-cp -rf $OVERRIDES_DIR/* "$WORK_DIR/"
