466648d054a2f1a9f20e982610487efdc2d1ccfc
park-brian <re.public@outlook.com>
2017-08-11 11:04:53 -0400

    use init-file in start_bash.sh

--- start_bash.sh
+++ start_bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WORK_DIR="environment"
-export PATH=$PATH:$PWD/$WORK_DIR/httpd/bin:$PWD/$WORK_DIR/php:$PWD/$WORK_DIR/mysql/bin:$PWD/$WORK_DIR/bin
+export PATH=$PWD/$WORK_DIR/httpd/bin:$PWD/$WORK_DIR/php:$PWD/$WORK_DIR/mysql/bin:$PWD/$WORK_DIR/bin:$PATH
 export COMPOSER_PROCESS_TIMEOUT=2000
 
-bash
+bash --init-file /etc/profile
