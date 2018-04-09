0f6a645d9cdac49dab07eaac59d98d76f240efad
Brian Park <park-brian@users.noreply.github.com>
2018-04-09 16:24:29 -0400

    start_mysqld.sh: set default authentication plugin

--- start_mysqld.sh
+++ start_mysqld.sh
@@ -4,4 +4,4 @@ source "$PWD/.bashrc"
 
 ## start mysqld
 echo "mysqld is now running [ctrl+c to exit]"
-mysqld.exe -u root --log_syslog=0
+mysqld.exe -u root --log_syslog=0 --default-authentication-plugin=mysql_native_password
