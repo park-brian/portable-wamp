7e30c6020698f75039e1c7a7549c4a6f26b26659
Brian Park <park-brian@users.noreply.github.com>
2020-06-22 02:35:12 -0400

    update shell.bat

--- shell.bat
+++ shell.bat
@@ -1,4 +1,5 @@
 @echo off
+rem Loads directories for command-line tools associated with httpd, mysql, and php into path
 set "PATH=%PATH%;%~dp0environment\bin;%~dp0environment\httpd\bin;%~dp0environment\php;%~dp0environment\mysql\bin;%~dp0environment\web\vendor\bin"
 set "COMPOSER_PROCESS_TIMEOUT=2000"
 cmd /k
