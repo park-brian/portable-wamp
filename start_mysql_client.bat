2858d3f2beb11c29b276c4fb22bb67cb2e30afce
Brian Park <park-brian@users.noreply.github.com>
2024-08-09 01:51:37 -0400

    update mysql start commands

--- start_mysql_client.bat
+++ start_mysql_client.bat
@@ -1,1 +1,2 @@
-"%~dp0environment\mysql\bin\mysql.exe" -u root
+echo Starting mysql client. If you are prompted for a password, and you have not set one, just press enter.
+"%~dp0environment\mysql\bin\mysql.exe" -u root -p
--- start_mysqld.bat
+++ start_mysqld.bat
@@ -1,4 +1,3 @@
 @echo off
 echo mysqld is now running [ctrl+c to exit]
-"%~dp0environment\mysql\bin\mysqld.exe" -u root --console --default-authentication-plugin=mysql_native_password
-
+"%~dp0environment\mysql\bin\mysqld.exe" --console
